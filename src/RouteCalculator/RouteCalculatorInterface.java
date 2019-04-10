20e14abaf36aa886589dcbdaa3b6cd4fc5239b4c
Johannes Nissen <nissen.johannes@gmail.com>
2019-04-10 17:03:28 +0200

    Corrected javaDoc in interface

--- src/RouteCalculator/RouteCalculatorInterface.java
+++ src/RouteCalculator/RouteCalculatorInterface.java
@@ -25,14 +25,6 @@ public interface RouteCalculatorInterface {
 	 * @param conPoint: ControlPoint in from of robot
 	 * @param pospoint: Rotation center of robot
 	 * @param destPoin:	Position of target tabletennis ball
-	 * @return: String-array that contains the command that will be sent to the robot
-	 * "0F:2;0G:200;0S:300;LR:40;RR:50;0B:true" 
-		0F = function
-		0G = grades
-		OS = Speed
-		LR = LeftRotation(grader)
-		RR = RightRotation(grader)
-		0B = boolean
 	 */
 	public void getDir(PointInGrid conPoint, PointInGrid pospoint, PointInGrid destPoint); //pos: current position, dest: destnation
 
