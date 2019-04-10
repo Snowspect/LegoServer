d862e3766e888f5e901dbac40880ba9a47bf8906
Stephan <Stephankhader@gmail.com>
2019-04-10 12:58:39 +0200

    Ryttet op

--- src/ServerRemote.java
+++ src/ServerRemote.java
@@ -114,17 +114,6 @@ public class ServerRemote {
 			turnLeft(400, 10, true);
 			//gyroSensor.reset();
 			break;
-		case RemoteCarClient.PRINTGYRO:
-			printGyro();
-			break;
-		
-		/*case RemoteCarClient.RIGHT:
-			A.rotateTo(-170);
-			break;
-		case RemoteCarClient.LEFT:
-			A.rotateTo(170);
-			break;
-		*/
 	}
 }
 
@@ -274,15 +263,7 @@ public void fullStop() { // q for activate
 	
 }
 
-/**
- * drive backwards
- */
-private void driveReverse() { // x for activate
-	motorLeft.setSpeed(500);
-	motorRight.setSpeed(500);
-	motorLeft.backward();
-	motorRight.backward();
-}
+
 
 /**
  * turns the robot to the left
@@ -372,105 +353,9 @@ private void unload(){
 	ArmWheelMoter.stop();
 }
 
-/**
- * Prints the gyro sensor value to console
- */
-public void printGyro() {
-	
-	final SampleProvider sp = gyroSensor.getAngleMode();
-	int value = 0;
-	float [] sample = new float[sp.sampleSize()];
-    sp.fetchSample(sample, 0);
-    value = (int)sample[0];
-	System.out.println("Iteration: " + value);
-	System.out.println("Gyro angle: " + value);
-	gyroSensor.reset();
-}
 
-/**
- * NEEDED?
- */
-public void rotateForward() {
 
-}
 
-/**
- * rotates the car around itself by an angle (to the right)
- * @param angle : the amount of degrees we turn around ourselves by gyro standard.
- */
-private void turnRightGyroImplementation(double angle) {
-
-	final SampleProvider sp = gyroSensor.getAngleMode();
-	int value = 0;
-	
-	motorLeft.setSpeed(300);
-	motorRight.setSpeed(300);
-	motorLeft.forward();
-	motorRight.backward();
-
-	while(true) {
-    	float [] sample = new float[sp.sampleSize()];
-        sp.fetchSample(sample, 0);
-        value = (int)sample[0];
-
-		if(value >= angle) {
-			motorLeft.setSpeed(0);
-			motorRight.setSpeed(0);
-			motorLeft.stop();
-			motorRight.stop();
-			System.out.println("Iteration: " + value);
-			System.out.println("Gyro angle: " + value);
-
-	        
-			if(value != 360) {
-				turnleftGyroImplementation(360-value);
-			}
-			
-			System.out.println("Resetting");
-			gyroSensor.reset();
-			break;
-		}
-	}
-	
-
-}
-
-/**
- * rotates the car around itself by an angle (to the left)
- * @param angle : the amount of degrees we turn around ourselves by gyro standard.
- */
-public void turnleftGyroImplementation(double angle) {
-	motorLeft.setSpeed(300);
-	motorRight.setSpeed(300);
-	motorLeft.backward();
-	motorRight.forward();
-
-	final SampleProvider sp = gyroSensor.getAngleMode();
-	int value = 0;
-
-	while(true) {
-    	float [] sample = new float[sp.sampleSize()];
-        sp.fetchSample(sample, 0);
-        value = (int)sample[0];
-		
-		if(value <= -angle) {
-			motorLeft.setSpeed(0);
-			motorRight.setSpeed(0);
-			motorLeft.stop();
-			motorRight.stop();
-			System.out.println("Iteration: " + value);
-			System.out.println("Gyro angle: " + value);
-			
-			if(value != -angle) {
-				turnRightGyroImplementation(-360+angle);
-				}
-			}
-			
-			System.out.println("Resetting");
-			gyroSensor.reset();
-			break;
-		}
-	}
 
 /**
  * Not implemented yet
